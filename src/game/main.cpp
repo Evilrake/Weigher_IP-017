690525f0d8cd8a18441a1f86c8c250cceeb369b0
Evilrake <kolyannevolyan@gmail.com>
2021-10-03 16:13:21 +0700

    makefile for simple.cpp

---
+++ src/game/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world" << endl;
+    system("pause");
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world" << endl;
+    system("pause");
+    return 0;
+}
