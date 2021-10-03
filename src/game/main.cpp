5073144c7e0be15243c665fb1ab9a7f25e99288a
Evilrake <kolyannevolyan@gmail.com>
2021-10-03 17:01:46 +0700

    makefile for simple  program

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -1,18 +1,23 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
-using namespace std;
 
 int main()
 {
-    cout << "Hello world" << endl;
-    system("pause");
-    return 0;
-}
-#include <iostream>
-using namespace std;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-int main()
-{
-    cout << "Hello world" << endl;
-    system("pause");
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
