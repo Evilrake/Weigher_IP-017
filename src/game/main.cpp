c86f0cf24e8394b6fb5df7239741cd6c4b47949b
KaPtErNiY <seregakonchakov85@gmail.com>
2021-10-05 23:11:57 +0700

    print secret word on screen

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -1,4 +1,4 @@
-#include <SFML/Graphics.hpp>
+﻿#include <SFML/Graphics.hpp>
 #include <game.h>
 #include <iostream>
 #include <menu.h>
@@ -12,8 +12,29 @@ int main()
 {
     RenderWindow window(sf::VideoMode(1000, 600), "Weigher game");
 
+    vector<vector<string>> words;
+
+    string easyWords[3][4]
+            = {{"фрукты", "яблоко", "банан", "груша"},
+               {"посуда", "тарелка", "кружка", "чашка"},
+               {"животные", "тигр", "олень", "лев"}};
+    string middleWords[3][4]
+            = {{"фрукты", "ананас", "мандарин", "слива"},
+               {"посуда", "поднос", "скалка", "кувшин"},
+               {"животные", "жираф", "панда", "кенгуру"}};
+    string hardWords[3][4]
+            = {{"фрукты", "авокадо", "манго", "папайя"},
+               {"посуда", "графин", "солонка", "противень"},
+               {"животные", "киви", "лемур", "геккон"}};
+
     int difficult = callMenu(window);
-    startGame(window, difficult);
+    if (difficult == 1) {
+        startGame(window, easyWords);
+    } else if (difficult == 2) {
+        startGame(window, middleWords);
+    } else {
+        startGame(window, hardWords);
+    }
 
     return 0;
 }
